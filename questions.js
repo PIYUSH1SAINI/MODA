040e12c4982b900c7320278c6a417f92bb4d3ee0
PIYUSH1SAINI <97388020+PIYUSH1SAINI@users.noreply.github.com>
2023-09-10 11:23:58 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "WHO WAS THE FIRST PRESIDENT OF INDIA ?",
+    answer: "DR.RAJENDRA PRASAD",
+    options: [
+      "DR.RAJENDRA PRASAD",
+      "RAM NATH KOVIND",
+      "PRANAB MUKHERJEE",
+      "JAWAHAR LAL NEHRU"
+    ]
+  
+  },
+    {
+    numb: 2,
+    question: "WHO IS THE FOUNDER OF FACEBOOK?",
+    answer: "MARK ZUCKERBERG",
+    options: [
+      "JAN KOUM",
+      "ELON MUSK",
+      "MARK ZUCKERBERG",
+      "JOHANN ZAHN"
+    ]
+  },
+    {
+    numb: 3,
+    question: "WHO INVENTED CAMERA?",
+    answer: "JOHANN ZAHN",
+    options: [
+      "ALBERT EINSTEIN",
+      "ALEXANDER GRAHAM BELL",
+      "ANTONIO MEUCCI",
+      "JOHANN ZAHN"
+    ]
+  },
+    {
+    numb: 4,
+    question: "WHO INVENTED TELEPHONE?",
+    answer: "ALEXANDER GRAHAM BELL",
+    options: [
+      "ALEXANDER GRAHAM BELL",
+      "CHARLES BABBAGE",
+      "THOMAS EDISON",
+      "ZACHARIAS JANSSEN"
+    ]
+  },
+    {
+      numb: 5,
+      question: "WHO INVENTED COMPUTER",
+      answer: "CHARLES BABBAGE",
+      options: [
+        "CHARLES BABBAGE",
+        "ELON MUSK",
+        "DOUGLAS ENGELBART",
+        "RENE SOMMER"
+      ]
+    }
+]
